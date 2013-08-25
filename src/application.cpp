00666b34a79a03ba0bb4638eff993d29197b2da2
Andreas Longva <andreas@fuwsoft.com>
2013-08-25 10:10:52 +0200

    Updated Application::run()

--- src/application.cpp
+++ src/application.cpp
@@ -37,51 +37,53 @@ Application::~Application()
 
 int Application::run()
 {
-    qmlRegisterUncreatableType<Player>("sonetta", 0, 1, "Player", "Can not instantiate Player from QML");
-    qmlRegisterUncreatableType<UIStateCoordinator>("sonetta", 0, 1, "UIStateCoordinator", "Can not instantiate UIStateCoordinator from QML.");
+    //    qmlRegisterUncreatableType<Player>("sonetta", 0, 1, "Player", "Can not instantiate Player from QML");
+    //    qmlRegisterUncreatableType<UIStateCoordinator>("sonetta", 0, 1, "UIStateCoordinator", "Can not instantiate UIStateCoordinator from QML.");
 
-    qmlRegisterType<Navigation>("sonetta", 0, 1, "Navigation");
-    qmlRegisterType<NavigationAttached>();
-    qmlRegisterUncreatableType<QuickNavEvent>("sonetta", 0, 1, "NavEvent", "Cannot instantiate navigation event. ");
+    //    qmlRegisterType<Navigation>("sonetta", 0, 1, "Navigation");
+    //    qmlRegisterType<NavigationAttached>();
+    //    qmlRegisterUncreatableType<QuickNavEvent>("sonetta", 0, 1, "NavEvent", "Cannot instantiate navigation event. ");
 
-//    qmlRegisterType<PlaylistContainerModel>("sonetta", 0, 1, "PlaylistContainerModel");
-//    qmlRegisterType<PlaylistModel>("sonetta", 0, 1, "PlaylistModel");
-    qmlRegisterType<AlbumListModel>("sonetta", 0, 1, "AlbumListModel");
-    qmlRegisterType<AlbumBrowseModel>("sonetta", 0, 1, "AlbumBrowseModel");
-    qmlRegisterType<QuickTrackInfo>("sonetta", 0, 1, "TrackInfo");
-    //qmlRegisterType<QuickArtistSynopsis>("sonetta", 0, 1, "ArtistSynopsis");
-    qmlRegisterType<QuickSearch>("sonetta", 0, 1, "Search");
+    ////    qmlRegisterType<PlaylistContainerModel>("sonetta", 0, 1, "PlaylistContainerModel");
+    ////    qmlRegisterType<PlaylistModel>("sonetta", 0, 1, "PlaylistModel");
+    //    qmlRegisterType<AlbumListModel>("sonetta", 0, 1, "AlbumListModel");
+    //    qmlRegisterType<AlbumBrowseModel>("sonetta", 0, 1, "AlbumBrowseModel");
+    //    qmlRegisterType<QuickTrackInfo>("sonetta", 0, 1, "TrackInfo");
+    //    //qmlRegisterType<QuickArtistSynopsis>("sonetta", 0, 1, "ArtistSynopsis");
+    //    qmlRegisterType<QuickSearch>("sonetta", 0, 1, "Search");
 
-    if (!m_spotify->createSession())
+    if (m_session->error() == sp::Error::Ok)
     {
-        qFatal("Failed to create spotify session. Aborting...");
+        m_nav = new Navigation(this);
+        m_view = new QQuickView;
+
+        QString applicationDir = applicationDirPath();
+
+        ImageProvider * provider = new ImageProvider(m_session, this);
+
+        m_view->engine()->addImageProvider(QLatin1String("sp"), provider);
+        m_view->engine()->rootContext()->setContextProperty("player", m_player);
+        m_view->engine()->rootContext()->setContextProperty("ui", m_ui);
+        m_view->engine()->addImportPath(applicationDir + QStringLiteral("/qml/modules"));
+        m_view->setSource(QUrl::fromLocalFile(applicationDir + QStringLiteral("/qml/rework/main.qml")));
+        m_view->setResizeMode(QQuickView::SizeRootObjectToView);
+
+        // Center view
+        QScreen * screen = m_view->screen();
+        QPoint screenCenter = screen->availableGeometry().center();
+        QPoint windowCenter = m_view->geometry().center();
+        m_view->setPosition(screenCenter - windowCenter);
+        m_view->showFullScreen();
+
+        // Start event loop
+        return exec();
+    }
+    else
+    {
+        const QByteArray msg = QByteArray("Session creation failed. Error: ") + sp::errorMessage(m_session->error()).toUtf8();
+        qFatal(msg.constData());
         return 1;
     }
-
-    m_nav = new Navigation(this);
-    m_view = new QQuickView;
-
-    QString applicationDir = applicationDirPath();
-
-    ImageProvider * provider = new ImageProvider(this);
-
-    m_view->engine()->addImageProvider(QLatin1String("sp"), provider);
-    m_view->engine()->rootContext()->setContextProperty("player", m_player);
-    m_view->engine()->rootContext()->setContextProperty("spotify", m_spotify);
-    m_view->engine()->rootContext()->setContextProperty("ui", m_ui);
-    m_view->engine()->addImportPath(applicationDir + QStringLiteral("/qml/modules"));
-    m_view->setSource(QUrl::fromLocalFile(applicationDir + QStringLiteral("/qml/rework/main.qml")));
-    m_view->setResizeMode(QQuickView::SizeRootObjectToView);
-
-    // Center view
-    QScreen * screen = m_view->screen();
-    QPoint screenCenter = screen->availableGeometry().center();
-    QPoint windowCenter = m_view->geometry().center();
-    m_view->setPosition(screenCenter - windowCenter);
-    m_view->showFullScreen();
-
-    // Start event loop
-    return exec();
 }
 
 Application * Application::instance()
